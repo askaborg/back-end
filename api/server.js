7fb635997a40f574131aae36ff92ae3ab700e46d
Vitaliy Mikhaylyuk <VitaliyMikhaylyuk@gmail.com>
2020-06-17 22:00:20 -0400

    server is running and works

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+
+//we will bring this in once the auth router is made
+// server.use('/api/auth', authRouter);
+
+server.get("/", (req, res) => {
+    res.status(200).json({ api: "up" });
+});
+
+module.exports = server;
+
