4ee76c8cb7633155a3072e54ce3f716b0a619db1
askaborg <hienle@askaborg.com>
2020-06-18 16:11:13 -0700

    Auth-complete

--- api/server.js
+++ api/server.js
@@ -1,22 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express")
+const cors = require("cors")
+const helmet = require("helmet")
 
 const authenticate = require("../auth/generateToken")
 const authRouter = require("../auth/auth-router")
-const server = express();
+const server = express()
 
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
 
 
 //we will bring this in once the auth router is made
-server.use('/api/auth', authRouter);
+server.use("/api/auth", authRouter)
 
 server.get("/", (req, res) => {
-    res.status(200).json({ api: "up" });
-});
+    res.status(200).json({ api: "up" })
+})
 
-module.exports = server;
-
+module.exports = server
